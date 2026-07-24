27afbfa799bc0f776d2daf69afede9b5e877b179
xuliang <jacky211@163.com>
2026-07-24 14:38:30 +0800

    fix: add deploy script and responding logics

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,9 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   reactStrictMode: true,
-  // Cloudflare does not support node: prefix
-  serverExternalPackages: [],
 };
-
-if (process.env.NODE_ENV === 'development') {
-  (async () => {
-    await setupDevPlatform();
-  })();
-}
 
 export default nextConfig;
